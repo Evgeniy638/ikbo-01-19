6a4f84fd132e2d670888ff45530f8099e8dabd1a
Evgeniy <Evgeniy>
2020-09-27 23:30:14 +0300

    added practice10

---
+++ practice10-SubbotinEvg/src/Client.java
@@ -0,0 +1,11 @@
+public class Client {
+    public Chair chair;
+
+    public void sit(){
+        System.out.println("Клиент сел на стул");
+    }
+
+    public void setChair(Chair chair) {
+        this.chair = chair;
+    }
+}
